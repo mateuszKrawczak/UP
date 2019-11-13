761d132d6928ecf23eca9d746e0de94ac6a19fb6
matson19 <241318@student.pwr.edu.pl>
2019-11-13 20:25:10 +0100

    Lab 2 - analizator sieci

---
+++ Lab 2 - analizator_sieci/lab18/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace lab18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxNapiece = new System.Windows.Forms.TextBox();
+            this.textBoxNatezenie = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMoc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.labelIP = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxNapiece
+            // 
+            this.textBoxNapiece.Location = new System.Drawing.Point(652, 157);
+            this.textBoxNapiece.Name = "textBoxNapiece";
+            this.textBoxNapiece.Size = new System.Drawing.Size(195, 26);
+            this.textBoxNapiece.TabIndex = 0;
+            // 
+            // textBoxNatezenie
+            // 
+            this.textBoxNatezenie.Location = new System.Drawing.Point(652, 224);
+            this.textBoxNatezenie.Name = "textBoxNatezenie";
+            this.textBoxNatezenie.Size = new System.Drawing.Size(195, 26);
+            this.textBoxNatezenie.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(935, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "U";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(935, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "A";
+            // 
+            // textBoxMoc
+            // 
+            this.textBoxMoc.Location = new System.Drawing.Point(652, 303);
+            this.textBoxMoc.Name = "textBoxMoc";
+            this.textBoxMoc.Size = new System.Drawing.Size(195, 26);
+            this.textBoxMoc.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(935, 309);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "kW";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(652, 78);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(195, 26);
+            this.textBoxIP.TabIndex = 7;
+            this.textBoxIP.Text = "169.254.2.10";
+            // 
+            // labelIP
+            // 
+            this.labelIP.AutoSize = true;
+            this.labelIP.Location = new System.Drawing.Point(932, 84);
+            this.labelIP.Name = "labelIP";
+            this.labelIP.Size = new System.Drawing.Size(24, 20);
+            this.labelIP.TabIndex = 8;
+            this.labelIP.Text = "IP";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(652, 368);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(231, 35);
+            this.buttonConnect.TabIndex = 9;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(932, 375);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(55, 20);
+            this.labelStatus.TabIndex = 10;
+            this.labelStatus.Text = "Offline";
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(652, 420);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(231, 37);
+            this.buttonDisconnect.TabIndex = 11;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 198);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(222, 26);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(313, 198);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(272, 26);
+            this.textBox2.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1065, 767);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.labelIP);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxMoc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNatezenie);
+            this.Controls.Add(this.textBoxNapiece);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNapiece;
+        private System.Windows.Forms.TextBox textBoxNatezenie;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMoc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.Label labelIP;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Lab 2 - analizator_sieci/lab18/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyModbus;
+
+namespace lab18
+{
+    public partial class Form1 : Form
+    {
+
+        ModbusClient modbusClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                modbusClient = new ModbusClient(textBoxIP.Text, 502);
+                modbusClient.UnitIdentifier = 0x01;
+                modbusClient.ConnectionTimeout = 350;
+                modbusClient.Connect();
+                labelStatus.Text = "Connected";
+                timer.Start();
+            } catch(Exception ex)
+            {
+                labelStatus.Text = ex.ToString();
+            }
+
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            modbusClient.Disconnect();
+            labelStatus.Text = "Offline";
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            
+            modbusClient.WriteMultipleCoils(4, new bool[] { true, true, true, true, true, true, true, true, true, true });    //Write Coils starting with Address 5
+            bool[] readCoils = modbusClient.ReadCoils(9, 10);                        //Read 10 Coils from Server, starting with address 10
+            int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(0, 10);    //Read 10 Holding Registers from Server, starting with Address 1
+
+            for (int i = 0; i < readCoils.Length; i++)
+            {
+                textBox1.AppendText("Value of Coil " + (9 + i + 1) + " " + readCoils[i].ToString());
+            }
+
+            for (int i = 0; i < readHoldingRegisters.Length; i++)
+            {
+                textBox2.AppendText("Value of HoldingRegister " + (i + 1) + " " + readHoldingRegisters[i].ToString());
+                modbusClient.Disconnect();
+            }
+
+            timer.Stop();
+        }
+
+    }
+}
