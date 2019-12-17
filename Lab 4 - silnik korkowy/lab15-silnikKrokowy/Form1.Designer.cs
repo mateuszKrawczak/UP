91691a6fd03d594b9f0b3a7d22726cfd0311f808
matson19 <241318@student.pwr.edu.pl>
2019-12-17 12:48:04 +0100

    sprawko update

---
+++ Lab 4 - silnik korkowy/lab15-silnikKrokowy/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace lab15_silnikKrokowy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.numericUpDownCounter = new System.Windows.Forms.NumericUpDown();
+            this.buttonRight2 = new System.Windows.Forms.Button();
+            this.buttonLeft2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownDegree = new System.Windows.Forms.NumericUpDown();
+            this.buttonRightHalf = new System.Windows.Forms.Button();
+            this.buttonLeftHalf = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCounter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDegree)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(464, 162);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(100, 59);
+            this.buttonLeft.TabIndex = 0;
+            this.buttonLeft.Text = "Left 1fazowy";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(464, 82);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(100, 65);
+            this.buttonRight.TabIndex = 1;
+            this.buttonRight.Text = "Prawo 1fazowy";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(464, 12);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(373, 43);
+            this.buttonConnect.TabIndex = 2;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(406, 308);
+            this.textBox.TabIndex = 3;
+            // 
+            // numericUpDownCounter
+            // 
+            this.numericUpDownCounter.Location = new System.Drawing.Point(602, 249);
+            this.numericUpDownCounter.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownCounter.Name = "numericUpDownCounter";
+            this.numericUpDownCounter.Size = new System.Drawing.Size(235, 26);
+            this.numericUpDownCounter.TabIndex = 4;
+            this.numericUpDownCounter.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numericUpDownCounter.ValueChanged += new System.EventHandler(this.numericUpDownCounter_ValueChanged);
+            // 
+            // buttonRight2
+            // 
+            this.buttonRight2.Location = new System.Drawing.Point(602, 82);
+            this.buttonRight2.Name = "buttonRight2";
+            this.buttonRight2.Size = new System.Drawing.Size(92, 65);
+            this.buttonRight2.TabIndex = 6;
+            this.buttonRight2.Text = "Right 2fazowy";
+            this.buttonRight2.UseVisualStyleBackColor = true;
+            this.buttonRight2.Click += new System.EventHandler(this.buttonRight2_Click);
+            // 
+            // buttonLeft2
+            // 
+            this.buttonLeft2.Location = new System.Drawing.Point(602, 162);
+            this.buttonLeft2.Name = "buttonLeft2";
+            this.buttonLeft2.Size = new System.Drawing.Size(92, 59);
+            this.buttonLeft2.TabIndex = 5;
+            this.buttonLeft2.Text = "Left 2fazowy";
+            this.buttonLeft2.UseVisualStyleBackColor = true;
+            this.buttonLeft2.Click += new System.EventHandler(this.buttonLeft2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(460, 251);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "SPEED :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(460, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "DEGREES :";
+            // 
+            // numericUpDownDegree
+            // 
+            this.numericUpDownDegree.Increment = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.numericUpDownDegree.Location = new System.Drawing.Point(602, 298);
+            this.numericUpDownDegree.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.numericUpDownDegree.Minimum = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            this.numericUpDownDegree.Name = "numericUpDownDegree";
+            this.numericUpDownDegree.Size = new System.Drawing.Size(235, 26);
+            this.numericUpDownDegree.TabIndex = 9;
+            this.numericUpDownDegree.Value = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.numericUpDownDegree.ValueChanged += new System.EventHandler(this.numericUpDownDegree_ValueChanged);
+            // 
+            // buttonRightHalf
+            // 
+            this.buttonRightHalf.Location = new System.Drawing.Point(732, 82);
+            this.buttonRightHalf.Name = "buttonRightHalf";
+            this.buttonRightHalf.Size = new System.Drawing.Size(105, 65);
+            this.buttonRightHalf.TabIndex = 10;
+            this.buttonRightHalf.Text = "Prawo Polkrokwy";
+            this.buttonRightHalf.UseVisualStyleBackColor = true;
+            this.buttonRightHalf.Click += new System.EventHandler(this.buttonRightHalf_Click);
+            // 
+            // buttonLeftHalf
+            // 
+            this.buttonLeftHalf.Location = new System.Drawing.Point(732, 162);
+            this.buttonLeftHalf.Name = "buttonLeftHalf";
+            this.buttonLeftHalf.Size = new System.Drawing.Size(105, 59);
+            this.buttonLeftHalf.TabIndex = 11;
+            this.buttonLeftHalf.Text = "Lewo Polkrokowy";
+            this.buttonLeftHalf.UseVisualStyleBackColor = true;
+            this.buttonLeftHalf.Click += new System.EventHandler(this.buttonLeftHalf_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 350);
+            this.Controls.Add(this.buttonLeftHalf);
+            this.Controls.Add(this.buttonRightHalf);
+            this.Controls.Add(this.numericUpDownDegree);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonRight2);
+            this.Controls.Add(this.buttonLeft2);
+            this.Controls.Add(this.numericUpDownCounter);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCounter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDegree)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.NumericUpDown numericUpDownCounter;
+        private System.Windows.Forms.Button buttonRight2;
+        private System.Windows.Forms.Button buttonLeft2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownDegree;
+        private System.Windows.Forms.Button buttonRightHalf;
+        private System.Windows.Forms.Button buttonLeftHalf;
+    }
+}
+
---
+++ Lab 4 - silnik korkowy/lab15-silnikKrokowy/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FTD2XX_NET;
+
+namespace lab15_silnikKrokowy
+{
+    public partial class Form1 : Form
+    {
+
+        FTD2XX_NET.FTDI.FT_STATUS ftstatus;
+        FTD2XX_NET.FTDI.FT_DEVICE_INFO_NODE[] devicelist = new FTD2XX_NET.FTDI.FT_DEVICE_INFO_NODE[1];
+        FTDI device = new FTDI();
+
+        byte[] LeftP = { 0x08, 0x02, 0x04, 0x01 };  // jednofazowe krokowe
+        byte[] RightP = { 0x01, 0x04, 0x02, 0x08 };
+
+        byte[] LeftD = { 0x09, 0x0A, 0x06, 0x05 }; //dwufazowe krokowe
+        byte[] RightD = { 0x05, 0x06, 0x0A, 0x09 };
+
+        byte[] LeftU = { 0x09, 0x08, 0x0A, 0x02, 0x06, 0x04, 0x05, 0x01 };  // polkrokowe
+        byte[] RightU = { 0x01, 0x05, 0x04, 0x06, 0x02, 0x0A, 0x08, 0x09 };
+
+        byte[] stop = { 0x00 };
+
+        int degree = 360; 
+        int speed = 50;   
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            textBox.Text += "Obrot w lewo...\r\n";
+            try
+            {
+                numericUpDownCounter.Enabled = false;
+                rotate(LeftP, 4);
+                numericUpDownCounter.Enabled = true;
+                textBox.Text += "Obrot w lewo dziala\r\n";
+            }
+            catch(Exception ex)
+            {
+                textBox.Text += "Nie Dziala\r\n";
+            }
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            textBox.Text += "Obrot w prawo...\r\n";
+            try
+            {
+                numericUpDownCounter.Enabled = false;
+                rotate(RightP, 4);
+                numericUpDownCounter.Enabled = true;
+                textBox.Text += "Obrot w prawo dziala\r\n";
+            }
+            catch (Exception ex)
+            {
+                textBox.Text += "Nie Dziala\r\n";
+            }
+        }
+
+        private void buttonRight2_Click(object sender, EventArgs e)
+        {
+            textBox.Text += "Obrot w prawo dwufazowy...\r\n";
+            try
+            {
+                numericUpDownCounter.Enabled = false;
+                rotate(RightD, 4);
+                numericUpDownCounter.Enabled = true;
+                textBox.Text += "Obrot w prawo dwufazowy dziala\r\n";
+            }
+            catch (Exception ex)
+            {
+                textBox.Text += "Nie Dziala\r\n";
+            }
+        }
+
+        private void buttonLeft2_Click(object sender, EventArgs e)
+        {
+            textBox.Text += "Obrot w lewo dwufazowy...\r\n";
+            try
+            {
+                numericUpDownCounter.Enabled = false;
+                rotate(LeftD, 4);
+                numericUpDownCounter.Enabled = true;
+                textBox.Text += "Obrot w lewo dwufazowy dziala\r\n";
+            }
+            catch (Exception ex)
+            {
+                textBox.Text += "Nie Dziala\r\n";
+            }
+        }
+
+        private void buttonRightHalf_Click(object sender, EventArgs e)
+        {
+            textBox.Text += "Obrot w prawo polkrokowy...\r\n";
+            try
+            {
+                numericUpDownCounter.Enabled = false;
+                rotate(RightU, 8);
+                numericUpDownCounter.Enabled = true;
+                textBox.Text += "Obrot w prawo polkrokowy dziala\r\n";
+            }
+            catch (Exception ex)
+            {
+                textBox.Text += "Nie Dziala\r\n";
+            }
+        }
+
+        private void buttonLeftHalf_Click(object sender, EventArgs e)
+        {
+            textBox.Text += "Obrot w lewo polkrokowy...\r\n";
+            try
+            {
+                numericUpDownCounter.Enabled = false;
+                rotate(LeftU, 8);
+                numericUpDownCounter.Enabled = true;
+                textBox.Text += "Obrot w lewo polkrokowy dziala\r\n";
+            }
+            catch (Exception ex)
+            {
+                textBox.Text += "Nie Dziala\r\n";
+            }
+        }
+
+        public void rotate(byte[] buff, int steps)
+        {
+            int loops = (degree * 10) / 74;
+
+            if (steps == 8)
+            {
+                loops *= 2;
+            }
+
+            Int32 bytesToWrite = 1;
+            UInt32 bytesWritten = 0;
+
+            byte[][] controlByte = new byte[steps][];
+
+            for (int i = 0; i < steps; i++)
+                controlByte[i] = new byte[] { buff[i] };
+
+            for (int k = 0, i = steps - 1; k < loops; k++)
+            {
+
+                ftstatus = device.Write(controlByte[i], bytesToWrite, ref bytesWritten); //przesłanie odpowiedniej kombinacji bitowej do sterownika
+                System.Threading.Thread.Sleep(1000 / speed);
+
+                if (i == 0)
+                    i = steps - 1;
+                else
+                    i--;
+            }
+
+            // wysłanie 0x00 w celu zaprzestanie wysylania stanów wysokich do silnika krokowego
+            ftstatus = device.Write(stop, 1, ref bytesWritten);
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            ftstatus = device.GetDeviceList(devicelist);
+            try
+            {
+                ftstatus = device.OpenByDescription(devicelist[0].Description);
+                ftstatus = device.SetBitMode(0xff, 1); //pozwala na przesylanie danych do ukladu
+                textBox.Text += "Status Polaczenia: " + ftstatus.ToString() + "\r\n";
+            }
+
+            catch (Exception ex)
+            {
+                textBox.Text += "Status Polaczenia: FT_FAIL \r\n";
+            }
+        }
+
+        private void numericUpDownCounter_ValueChanged(object sender, EventArgs e)
+        {
+            speed = Convert.ToInt32(numericUpDownCounter.Value);
+        }
+
+        private void numericUpDownDegree_ValueChanged(object sender, EventArgs e)
+        {
+            degree = Convert.ToInt32(numericUpDownDegree.Value);
+        }
+
+
+    }
+}
