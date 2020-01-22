2dd8f918171e39df1116060e96db2b382c249a20
matson19 <241318@student.pwr.edu.pl>
2020-01-22 12:15:31 +0100

    update lab6

---
+++ Lab 6 - gps/GPS/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace GPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.textBoxLatitude = new System.Windows.Forms.TextBox();
+            this.textBoxLongitude = new System.Windows.Forms.TextBox();
+            this.textBoxHigh = new System.Windows.Forms.TextBox();
+            this.textBoxSatelites = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.labelConnectionStatus = new System.Windows.Forms.Label();
+            this.textBoxTime = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(63, 40);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(138, 63);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Połącz";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Location = new System.Drawing.Point(339, 40);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(138, 63);
+            this.buttonRead.TabIndex = 1;
+            this.buttonRead.Text = "Odczyt";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(607, 40);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(138, 63);
+            this.buttonShow.TabIndex = 2;
+            this.buttonShow.Text = "Pokaż na mapie";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // textBoxLatitude
+            // 
+            this.textBoxLatitude.Location = new System.Drawing.Point(339, 474);
+            this.textBoxLatitude.Name = "textBoxLatitude";
+            this.textBoxLatitude.Size = new System.Drawing.Size(273, 26);
+            this.textBoxLatitude.TabIndex = 3;
+            // 
+            // textBoxLongitude
+            // 
+            this.textBoxLongitude.Location = new System.Drawing.Point(339, 524);
+            this.textBoxLongitude.Name = "textBoxLongitude";
+            this.textBoxLongitude.Size = new System.Drawing.Size(273, 26);
+            this.textBoxLongitude.TabIndex = 4;
+            // 
+            // textBoxHigh
+            // 
+            this.textBoxHigh.Location = new System.Drawing.Point(339, 582);
+            this.textBoxHigh.Name = "textBoxHigh";
+            this.textBoxHigh.Size = new System.Drawing.Size(273, 26);
+            this.textBoxHigh.TabIndex = 5;
+            // 
+            // textBoxSatelites
+            // 
+            this.textBoxSatelites.Location = new System.Drawing.Point(339, 638);
+            this.textBoxSatelites.Name = "textBoxSatelites";
+            this.textBoxSatelites.Size = new System.Drawing.Size(273, 26);
+            this.textBoxSatelites.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 474);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Szerokosc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 527);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Dlugosc";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(184, 585);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Wysokosc";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(184, 641);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Satelity";
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(63, 189);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxMessage.Size = new System.Drawing.Size(682, 205);
+            this.textBoxMessage.TabIndex = 15;
+            // 
+            // labelConnectionStatus
+            // 
+            this.labelConnectionStatus.AutoSize = true;
+            this.labelConnectionStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelConnectionStatus.ForeColor = System.Drawing.Color.Red;
+            this.labelConnectionStatus.Location = new System.Drawing.Point(250, 130);
+            this.labelConnectionStatus.Name = "labelConnectionStatus";
+            this.labelConnectionStatus.Size = new System.Drawing.Size(337, 37);
+            this.labelConnectionStatus.TabIndex = 16;
+            this.labelConnectionStatus.Text = "Status : Niepołączono";
+            // 
+            // textBoxTime
+            // 
+            this.textBoxTime.Location = new System.Drawing.Point(339, 418);
+            this.textBoxTime.Name = "textBoxTime";
+            this.textBoxTime.Size = new System.Drawing.Size(273, 26);
+            this.textBoxTime.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(184, 421);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Czas GMT0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 727);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxTime);
+            this.Controls.Add(this.labelConnectionStatus);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSatelites);
+            this.Controls.Add(this.textBoxHigh);
+            this.Controls.Add(this.textBoxLongitude);
+            this.Controls.Add(this.textBoxLatitude);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.buttonRead);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.TextBox textBoxLatitude;
+        private System.Windows.Forms.TextBox textBoxLongitude;
+        private System.Windows.Forms.TextBox textBoxHigh;
+        private System.Windows.Forms.TextBox textBoxSatelites;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Label labelConnectionStatus;
+        private System.Windows.Forms.TextBox textBoxTime;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Lab 6 - gps/GPS/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.IO;
+using System.IO.Ports;
+using System.Net;
+using System.Reflection;
+using System.Threading;
+
+
+namespace GPS
+{
+    public partial class Form1 : Form
+    {
+
+        static SerialPort serialPort;
+        string outputData;
+        string latitude;
+        string longitude;
+        bool connected = false;
+        char[] receivedData = new char[512];
+
+        public Form1()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
+            InitializeComponent();
+            serialPort = new SerialPort();
+            textBoxMessage.Text = "";
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (!connected)
+            {
+                serialPort.PortName = "COM8";
+                serialPort.BaudRate = 9600;
+                serialPort.Open();
+                connected = true;
+                labelConnectionStatus.Text = "Status : Połączono";
+                labelConnectionStatus.ForeColor = Color.Green;
+                ReadData();
+            }
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            ClearData();
+            if (connected)
+            {
+                ReadData();
+            }
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StringBuilder queryAddress = new StringBuilder();
+                queryAddress.Append("http://maps.google.com/maps?q=");
+
+                if (latitude != string.Empty)
+                {
+                    queryAddress.Append(latitude + "%2C");
+                }
+
+                if (longitude != string.Empty)
+                {
+                    queryAddress.Append(longitude);
+                }
+
+                System.Diagnostics.Process.Start(queryAddress.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+        }
+
+        private void ReadData()
+        {
+
+            outputData = serialPort.ReadExisting();
+
+            var splitedData = outputData.Split('$');
+
+            foreach (var line in splitedData)
+            {
+                try
+                {
+                    if (line.Contains("GPGGA"))
+                    {
+
+
+                        string fLatitude = "";
+                        string fLongitude = "";
+
+                        var info = line.Split(',');
+
+                        double longdec = double.Parse(info[4], CultureInfo.InvariantCulture) / 100.0;
+                        double latdec = double.Parse(info[2], CultureInfo.InvariantCulture) / 100.0;
+
+                        if (info[3] == "S")
+                        {
+                            fLatitude = "-";
+                        }
+                        if (info[5] == "W")
+                        {
+                            fLongitude = "-";
+                        }
+
+                        var latSplitted = Convert.ToString(latdec).Split('.');
+                        var longSplitted = Convert.ToString(longdec).Split('.');
+
+                        longdec = Convert.ToDouble("0." + longSplitted[1], CultureInfo.InvariantCulture) * 10 / 6;
+                        latdec = Convert.ToDouble("0." + latSplitted[1], CultureInfo.InvariantCulture) * 10 / 6;
+
+                        textBoxLatitude.Text = fLatitude + (Convert.ToDouble(latSplitted[0]) + latdec).ToString("F6");
+                        textBoxLongitude.Text = fLongitude + (Convert.ToDouble(longSplitted[0]) + longdec).ToString("F6");
+
+                        latitude = fLatitude + (Convert.ToDouble(latSplitted[0]) + latdec).ToString("F6");
+                        longitude = fLongitude + (Convert.ToDouble(longSplitted[0]) + longdec).ToString("F6");
+
+                        textBoxTime.Text = info[1].Substring(0, 2) + ":" + info[1].Substring(2,2) + ":" + info[1].Substring(4,2);
+
+                        textBoxMessage.Text += "$" + line;
+                        textBoxHigh.Text = info[9] + " m";
+                        textBoxSatelites.Text = info[7];
+
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("ERROR");
+                }
+            }
+        }
+
+        private void ClearData()
+        {
+            textBoxLatitude.Text = "";
+            textBoxLongitude.Text = "";
+            textBoxMessage.Text += "";
+            textBoxHigh.Text = "";
+            textBoxSatelites.Text = "";
+        }
+
+    }
+}
